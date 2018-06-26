a0bfa35984d8451e87ba4c64202f47b90be59220
kuangcp <kuangcp@aliyun.com>
2018-06-26 10:52:14 +0800

    test log

---
+++ java-classfile/src/main/java/log/CorrectLog.java
@@ -0,0 +1,65 @@
+package log;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by https://github.com/kuangcp
+ * find correct log way by use logback
+ * @author kuangcp
+ */
+@Slf4j
+public class CorrectLog {
+
+  /**
+   * F G H L is correct, but G & L often use
+   *
+   * log.error("", e);
+   * log.error("{}", "Error : ", e);
+   */
+  public static void main(String[] a) {
+    try {
+      double num = 1/0;
+      System.out.println(num);
+    } catch (Exception e) {
+      // compile error
+//      log.error(e); //A
+//      log.error(e, e); //B
+
+      // Exception ClassName and message
+      log.debug("C");
+      log.error("" + e); //C
+      log.debug("D");
+      log.error(e.toString()); //D
+
+      // just message
+      log.debug("E");
+      log.error(e.getMessage()); //E
+
+      log.debug("F");
+      log.error(null, e); //F
+
+      log.debug("G");
+      log.error("", e); //G
+
+      log.debug("H");
+      log.error("{}", e); //H
+
+      // just message
+      log.debug("I");
+      log.error("{}", e.getMessage()); //I
+
+      // Exception ClassName and message
+      log.debug("J");
+      log.error("Error : " + e); //J
+
+      // just message
+      log.debug("K");
+      log.error("Error : " + e.getMessage()); //K
+
+      log.debug("L");
+      log.error("{}", "Error : ", e); //L
+    }
+  }
+
+
+}
