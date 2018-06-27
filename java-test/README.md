61021180a7f0399efd744cbccd59e878dee6d35a
kuangcp <kuangcp@aliyun.com>
2018-06-27 18:31:33 +0800

    add module java-test

---
+++ java-test/README.md
@@ -0,0 +1,8 @@
+# Java Test
+
+> Java 测试相关 学习使用 Junit TestNG
+
+- [ ] 如何让测试代码只读取test下的resources
+- [ ] 如何书写有效的测试代码
+
+
---
+++ java-test/src/main/java/com/github/kuangcp/domain/Person.java
@@ -0,0 +1,14 @@
+package com.github.kuangcp.domain;
+
+import lombok.Data;
+
+/**
+ * Created by https://github.com/kuangcp
+ *
+ * @author kuangcp
+ */
+@Data
+public class Person {
+
+  private String name;
+}
