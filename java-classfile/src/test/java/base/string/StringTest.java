b66a46a09f919435cdfffa38f813b0dc1a9c941f
kuangcp <kuangcp@aliyun.com>
2018-07-09 21:14:01 +0800

    实验, 记住很多细节性知识

--- java-classfile/src/test/java/base/string/StringTest.java
+++ java-classfile/src/test/java/base/string/StringTest.java
@@ -1,11 +1,42 @@
 package base.string;
 
+import java.util.ArrayList;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
 /**
  * Created by https://github.com/kuangcp
  *
  * @author kuangcp
  * @date 18-4-4  上午11:32
  */
+@Slf4j
 public class StringTest {
 
+  // Forced type conversion
+  @Test
+  public void testConvert(){
+    List<Object> list = new ArrayList<>();
+    list.add(1);
+    Object name = list.get(0);
+    log.debug("name: {}", name);
+    log.debug("is null : {}", name == null);
+    log.debug("is String : {}", name instanceof String);
+    log.debug("is int : {}", name instanceof Integer);
+
+    list.add(null);
+    list.add(null);
+
+    log.debug("list.size {}", list.size());
+
+    if(list.get(0) instanceof String){
+      String nullToString = (String)list.get(0);
+      log.debug("string: {}", nullToString);
+    }else{
+      log.debug("not String : {}", list.get(0));
+    }
+  }
+
+  // TODO instanceof 会影响性能么?
 }
--- java-classfile/src/test/java/com/github/kuangcp/read/ClassScannerTest.java
+++ java-classfile/src/test/java/com/github/kuangcp/read/ClassScannerTest.java
@@ -1,43 +1,42 @@
 package com.github.kuangcp.read;
 
-import org.junit.Test;
-
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 
 
 /**
  * Created by https://github.com/kuangcp
  *
  * @author kuangcp
- * @date 18-5-21  下午2:18
  */
+@Slf4j
 public class ClassScannerTest {
 
-    @Test
-    public void testGetPackageAllClasses() throws Exception {
-        // 递归找到当前类所在包的所有Logic后缀且继承了抽象逻辑类, 然后就能生成代码了
-        List<String> list = new ArrayList<>();
-        list.add("*Base");
-        ClassScanner scaner = new ClassScanner(true, true, list);
-        System.out.println(ClassScannerTest.class.getPackage().getName());
-        Set<Class<?>> clazzSet = scaner.getPackageAllClasses(ClassScannerTest.class.getPackage().getName(), true);
-        System.out.println(clazzSet.size());
-        Iterator<Class<?>> iterator = clazzSet.iterator();
-        while (iterator.hasNext()) {
-            Class<?> clazz = iterator.next();
-            if (clazz != BaseFather.class && BaseFather.class.isAssignableFrom(clazz)) { // 继承自 BaseFather
-                String Name = clazz.getSimpleName();
-                String name = lowerCaseFirstLetter(Name);
-                System.out.println(String.format("    public final static %s %s = new %s();", Name, name, Name));
-            }
-
-        }
+  @Test
+  public void testGetPackageAllClasses() throws Exception {
+    // 递归找到当前类所在包的所有 Base 后缀且继承了抽象逻辑类, 然后生成代码
+    List<String> list = new ArrayList<>();
+    list.add("*Base");
+    ClassScanner scanner = new ClassScanner(true, true, list);
+    System.out.println(ClassScannerTest.class.getPackage().getName());
+    Set<Class<?>> clazzSet = scanner
+        .getPackageAllClasses(ClassScannerTest.class.getPackage().getName(), true);
+    log.info("size: {}", clazzSet.size());
+    for (Class<?> clazz : clazzSet) {
+      if (clazz != BaseFather.class && BaseFather.class.isAssignableFrom(clazz)) { // 继承自 BaseFather
+        String rawName = clazz.getSimpleName();
+        String name = lowerCaseFirstLetter(rawName);
+        System.out.println(String.format
+            ("    public final static %s %s = new %s();", rawName, name, rawName));
+      }
     }
+  }
 
-    private static String lowerCaseFirstLetter(String name) {
-        return name.length() == 1 ? name.toLowerCase() : name.substring(0, 1).toLowerCase() + name.substring(1);
-    }
+  private static String lowerCaseFirstLetter(String name) {
+    return name.length() == 1 ? name.toLowerCase()
+        : name.substring(0, 1).toLowerCase() + name.substring(1);
+  }
 }
