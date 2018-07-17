3d92e27dda1c9f617e46d25156cb65b575f886b6
kuangcp <kuangcp@aliyun.com>
2018-07-17 21:34:02 +0800

    迭代都忘了

---
+++ java-concurrents/src/main/java/redis/migration/Main.java
@@ -0,0 +1,14 @@
+package redis.migration;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author kuangcp on 18-7-17-上午10:42
+ */
+@Slf4j
+public class Main {
+
+  public static void main(String[] args){
+    log.debug("debug : {}");
+  }
+}
---
+++ java-concurrents/src/main/java/redis/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Redis 相关操作
+ * @author kuangcp on 18-7-17-上午10:34
+ */
+package redis;
