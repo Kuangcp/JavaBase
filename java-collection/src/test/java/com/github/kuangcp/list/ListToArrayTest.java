e6c866459642369891ef3967fd7aef3e82e676a0
mythos <kuangcp@aliyun.com>
2018-08-06 00:14:21 +0800

    re set package

--- java-collection/src/test/java/com/github/kuangcp/list/ListToArrayTest.java
+++ java-collection/src/test/java/com/github/kuangcp/list/ListToArrayTest.java
@@ -2,15 +2,14 @@ package com.github.kuangcp.list;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 
 /**
  * Created by https://github.com/kuangcp on 17-8-20  下午3:02
  */
-public class LearnArrayList {
-
-  public static void main(String[] s) {
-    toArrayMethod();
-  }
+@Slf4j
+public class ListToArrayTest {
 
   /**
    * 测试toArray方法，得到的结论就是 无参数那个是复制一份返回一个等大的数组回来
@@ -18,24 +17,25 @@ public class LearnArrayList {
    * 但是若空间小了就采取无参的做法新建一个
    * 若空间大了 那么会将结果数组最后一个元素之后的元素设置为 null。
    */
-  private static void toArrayMethod() {
+  @Test
+  public void toArrayMethod() {
     ArrayList<Integer> list = new ArrayList<>();
     for (int i = 0; i < 10; i++) {
       list.add(i);
     }
-    System.out.println(list);
+    log.debug("{}", list);
     Object[] copy1 = list.toArray();
-    System.out.println(Arrays.toString(copy1));
+    log.debug(Arrays.toString(copy1));
 
     Integer[] target = new Integer[10];
-    System.out.println(Arrays.toString(target));
+    log.debug(Arrays.toString(target));
     Object[] copy2 = list.toArray(target);
-    System.out.println(Arrays.toString(copy2));
+    log.debug(Arrays.toString(copy2));
 
     Integer[] target2 = new Integer[5];
-    System.out.println(Arrays.toString(target2));
+    log.debug(Arrays.toString(target2));
     Object[] copy3 = list.toArray(target2);
-    System.out.println(Arrays.toString(copy3));
+    log.debug(Arrays.toString(copy3));
   }
 
 }
---
+++ java-collection/src/test/java/com/github/kuangcp/map/LearnMap.java
@@ -0,0 +1,57 @@
+package com.github.kuangcp.map;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Created by https://github.com/kuangcp on 17-8-21  下午2:48
+ * Map 的常见迭代用法
+ */
+@Slf4j
+public class LearnMap {
+  private static Map<String, String> map = new HashMap<>();
+
+  @BeforeClass
+  public static void init() {
+    map.put("1", "a");
+    map.put("2", "b");
+    map.put("3", "c");
+  }
+
+  /**
+   * 通过Map.keySet()遍历key和value,二次取值
+   */
+  @Test
+  public void loopByKeySet() {
+    for (String key : map.keySet()) {
+      log.debug("{}:{}", key, map.get(key));
+    }
+  }
+
+  /**
+   * 通过Map.entrySet()遍历key和value(推荐使用)
+   */
+  @Test
+  public void loopByEntrySet() {
+    for (Map.Entry<String, String> entry : map.entrySet()) {
+      log.debug("{}:{}", entry.getKey(), entry.getValue());
+    }
+  }
+
+  /**
+   * 通过Map.entrySet()使用iterator()遍历key和value
+   * idea 都会提出警告, 说明了这种方式不是很好的遍历方式
+   */
+  @Test
+  public void loopByIterator() {
+    Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
+    while (iterator.hasNext()) {
+      Map.Entry<String, String> entry = iterator.next();
+      log.debug("{}:{}", entry.getKey(), entry.getValue());
+    }
+  }
+}
