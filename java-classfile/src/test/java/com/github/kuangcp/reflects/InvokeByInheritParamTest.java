ec48ec93c7a301eb38da7d85eb920e47fcf58350
mythos <kuangcp@aliyun.com>
2018-11-25 11:18:19 +0800

    change file location

--- java-classfile/src/test/java/com/github/kuangcp/reflects/InvokeByInheritParamTest.java
+++ java-classfile/src/test/java/com/github/kuangcp/reflects/InvokeByInheritParamTest.java
@@ -4,7 +4,6 @@ import static com.github.kuangcp.time.GetRunTime.GET_RUN_TIME;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
-import com.github.kuangcp.time.GetRunTime;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import lombok.Data;
@@ -16,8 +15,9 @@ import org.junit.Test;
 /**
  * Created by https://github.com/kuangcp
  *
- * 也就是说,可以让参数具有多态性, 在反射的时候 参数的类型是一一对应的不存在多态,
+ * 也就是说,可以让参数具有多态性, 但是在反射的时候 参数的类型是一一对应的, 不存在多态
  * 当拿到了方法之后, invoke的时候按需放入参数就没有问题
+ *
  * @author kuangcp
  */
 @Slf4j
@@ -77,32 +77,31 @@ public class InvokeByInheritParamTest {
     }
   }
 
-  // 通过两次反射获取 method 并执行
+  /**
+   * 反射时, 其方法的参数不存在多态的概念
+   */
   @Test
-  public void testInvokeTryTwice() {
+  public void testInvokeTryTwice() throws NoSuchMethodException {
+    Method method;
     try {
-      Method method;
-
-      // 反射时, 其参数不存在多态的概念
-      try {
-        // 所以这里会必然报错
-        method = Logic.class.getDeclaredMethod("isFailed", RunParam.class);
-      } catch (NoSuchMethodException e) {
-        method = Logic.class.getDeclaredMethod("isFailed", CommonParam.class);
-      }
-
-      verifySuite(method);
+      // 所以这里会必然报错
+      method = Logic.class.getDeclaredMethod("isFailed", RunParam.class);
     } catch (NoSuchMethodException e) {
-      e.printStackTrace();
+      method = Logic.class.getDeclaredMethod("isFailed", CommonParam.class);
     }
+
+    verifySuite(method);
   }
 
+  /**
+   * 经过测试发现 通过异常来控制逻辑, 耗时是直接反射 的 4-7 倍
+   */
   @Test
   @Ignore
-  public void comparePerformance() {
+  public void comparePerformance() throws NoSuchMethodException {
 
     int concurrent = 10;
-    // 经过测试发现 使用异常来控制逻辑, 耗时是直接反射 的 4-7 倍
+
     GET_RUN_TIME.startCount();
     for (int i = 0; i < concurrent; i++) {
       testInvokeTryTwice();
