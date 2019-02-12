3c3933031c7cd53c362dec8d8f43a05eaf22f59d
kuangcp <kuangcp@aliyun.com>
2019-02-12 18:04:43 +0800

    optimized naming

--- java-class/src/test/java/com/github/kuangcp/reflects/ReflectPerformanceTest.java
+++ java-class/src/test/java/com/github/kuangcp/reflects/ReflectPerformanceTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 
 /**
  * 反射的性能问题 http://www.cnblogs.com/zhishan/p/3195771.html
- * cglib 耗时 50%-70% 于缓存, 10% 于 原始方式
+ * cglib 耗时 50%-70% 于缓存, 10% 于 原始方式 (已缓存)
  *
  * @author kuangcp
  */
@@ -20,7 +20,7 @@ public class ReflectPerformanceTest {
   private static final int LOOP_SIZE = 5000_000;
 
   @Data
-  class TestClass {
+  class TargetObject {
 
     private int num;
   }
@@ -29,68 +29,68 @@ public class ReflectPerformanceTest {
   @Test
   public void testGetSet() {
     long sum = 0;
-    TestClass testClass = new TestClass();
+    TargetObject targetObject = new TargetObject();
 
     GetRunTime.GET_RUN_TIME.startCount();
     for (int i = 0; i < LOOP_SIZE; ++i) {
-      testClass.setNum(i);
-      sum += testClass.getNum();
+      targetObject.setNum(i);
+      sum += targetObject.getNum();
     }
     GetRunTime.GET_RUN_TIME.endCountOneLine("invoke get-set method ");
-//    log.info("LOOP_SIZE {} 和是 {} ", LOOP_SIZE, sum);
+    log.info("LOOP_SIZE {} 和是 {} ", LOOP_SIZE, sum);
   }
 
   @Test
   public void testOriginReflect() throws Exception {
     long sum = 0;
-    TestClass testClass = new TestClass();
+    TargetObject targetObject = new TargetObject();
 
     GetRunTime.GET_RUN_TIME.startCount();
     for (int i = 0; i < LOOP_SIZE; ++i) {
-      Method method = testClass.getClass().getMethod("setNum", int.class);
-      method.invoke(testClass, i);
-      sum += testClass.getNum();
+      Method method = targetObject.getClass().getMethod("setNum", int.class);
+      method.invoke(targetObject, i);
+      sum += targetObject.getNum();
     }
 
     GetRunTime.GET_RUN_TIME.endCountOneLine("simple reflect ");
-//    log.info("LOOP_SIZE {} 和是 {} ", LOOP_SIZE, sum);
+    log.info("LOOP_SIZE {} 和是 {} ", LOOP_SIZE, sum);
   }
 
   @Test
   public void testOriginReflectWithCache() throws Exception {
     long sum = 0;
-    TestClass testClass = new TestClass();
+    TargetObject targetObject = new TargetObject();
     GetRunTime.GET_RUN_TIME.startCount();
 
-    Method method = testClass.getClass().getMethod("setNum", int.class);
+    Method method = targetObject.getClass().getMethod("setNum", int.class);
     for (int i = 0; i < LOOP_SIZE; ++i) {
-      method.invoke(testClass, i);
-      int num = testClass.getNum();
+      method.invoke(targetObject, i);
+      int num = targetObject.getNum();
       sum += num;
     }
 
     GetRunTime.GET_RUN_TIME.endCountOneLine("simple reflect with cache");
-//    log.info("LOOP_SIZE {} 和是 {} ", LOOP_SIZE, sum);
+    log.info("LOOP_SIZE {} 和是 {} ", LOOP_SIZE, sum);
   }
 
   @Test
   public void testCglib() throws Exception {
     long sum = 0;
-    TestClass testClass = new TestClass();
+    TargetObject targetObject = new TargetObject();
 
-    FastClass testClazz = FastClass.create(TestClass.class);
+    FastClass testClazz = FastClass.create(TargetObject.class);
     FastMethod method = testClazz.getMethod("setNum", new Class[]{int.class});
 
     Object[] param = new Object[1];
     GetRunTime.GET_RUN_TIME.startCount();
     for (int i = 0; i < LOOP_SIZE; ++i) {
       param[0] = i;
-      method.invoke(testClass, param);
-      sum += testClass.getNum();
+      method.invoke(targetObject, param);
+      sum += targetObject.getNum();
     }
 
     GetRunTime.GET_RUN_TIME.endCountOneLine("use cglib ");
-//    log.info("LOOP_SIZE {} 和是 {} ", LOOP_SIZE, sum);
+    log.info("LOOP_SIZE {} 和是 {} ", LOOP_SIZE, sum);
   }
 
 }
