75ebb6a9496252ee92e72a07703aced019ed5ad5
kuangcp <kuangcp@aliyun.com>
2019-02-14 15:33:44 +0800

    optimize

--- java-8/src/test/java/com/github/kuangcp/function/sort/BaseTypeSortTest.java
+++ java-8/src/test/java/com/github/kuangcp/function/sort/BaseTypeSortTest.java
@@ -3,38 +3,42 @@ package com.github.kuangcp.function.sort;
 
 import static com.github.kuangcp.time.GetRunTime.GET_RUN_TIME;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.Test;
 
 /**
  * @author kuangcp on 18-8-17-下午12:00
  * base data type use functional Comparator to sort
+ *
+ * Double Float is the same way
  */
 public class BaseTypeSortTest {
 
   @Test
   public void testInt() {
-    int[] array = {2, 4, 5, 3, 1, 5, 6, 8, 3, 6, 7, 12, 556};
+    List<Integer> lists = IntStream.rangeClosed(1, 10000)
+        .map(i -> ThreadLocalRandom.current().nextInt(10000)).boxed()
+        .collect(Collectors.toList());
 
-    ArrayList<Integer> lists = new ArrayList<>();
-    for (int num : array) {
-      lists.add(num);
-    }
     GET_RUN_TIME.startCount();
-
     lists.sort(Comparator.comparingInt(Integer::intValue));
-    lists.forEach(System.out::println);
     GET_RUN_TIME.endCount("排序完成");
   }
-  
+
+  // TODO 一起执行时 为什么这个后执行的方法要快, 单独执行就不会有这样的情况
   @Test
-  public void testLong(){
-    
-  }
-  
-  @Test
-  public void testDouble(){
-    
+  public void testLong() {
+    List<Long> lists = IntStream.rangeClosed(1, 10000)
+        .mapToLong(i -> ThreadLocalRandom.current().nextLong(10000)).boxed()
+        .collect(Collectors.toList());
+
+    GET_RUN_TIME.startCount();
+    lists.sort(Comparator.comparingLong(Long::longValue));
+    GET_RUN_TIME.endCount("排序完成");
   }
 }
