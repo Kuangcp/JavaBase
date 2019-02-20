a7269c276b35375db5bc45bb4de756b209633f05
kuangcp <kuangcp@aliyun.com>
2019-02-20 19:40:42 +0800

    format code

--- java-netty/src/main/java/netty/timeServer/TimeServerHandler.java
+++ java-netty/src/main/java/netty/timeServer/TimeServerHandler.java
@@ -4,10 +4,14 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 服务端的业务代码 继承自 ChannelHandlerAdapter
  */
+@Slf4j
 public class TimeServerHandler extends ChannelHandlerAdapter {
 
   /**
@@ -18,10 +22,12 @@ public class TimeServerHandler extends ChannelHandlerAdapter {
     ByteBuf buf = (ByteBuf) msg;
     byte[] req = new byte[buf.readableBytes()];
     buf.readBytes(req);
-    String body = new String(req, "UTF-8");
-    System.out.println("收到  : " + body);
-    String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(
-        System.currentTimeMillis()).toString() : "BAD ORDER";
+    String body = new String(req, StandardCharsets.UTF_8);
+
+    log.info("server receive msg: {}", body);
+
+    String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ?
+        LocalDateTime.now().toString() : "BAD ORDER";
     ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
     ctx.write(resp);
   }
