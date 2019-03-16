e723df36a0e5aafa795331158c9e05ebfed0b951
mythos <kuangcp@aliyun.com>
2019-03-17 01:12:21 +0800

    *) copy file

--- java-io/src/main/java/com/github/kuangcp/inout/CopyFile.java
+++ java-io/src/main/java/com/github/kuangcp/inout/CopyFile.java
@@ -1,6 +1,15 @@
 package com.github.kuangcp.inout;
 
-import java.io.*;
+import com.github.kuangcp.io.ResourceTool;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 关于相对路径文件的复制问题
@@ -9,64 +18,52 @@ import java.io.*;
  * InputStream   所有输入流基类（建立流）（字节流）
  * InputStreamReader 将字节流  转换成 字符流
  * BufferedReader  从字符流输入流读取文件
- *
  */
-public class CopyFile {
+@Slf4j
+class CopyFile {
 
-	public static void main(String[] args) {
-		InputStream input = null;
-		BufferedReader br = null;
-		InputStreamReader ids =null;
-		BufferedWriter bw = null;
-		OutputStream out = null;
-		OutputStreamWriter os = null;
+  // 字节流 字符流 读入 ->
+  void copyFileWithSixChannel(String from, String dest) {
+    InputStream inputStream = null;
+    BufferedReader bufferedReader = null;
+    InputStreamReader streamReader = null;
 
+    BufferedWriter bufferedWriter;
+    OutputStream outputStream = null;
+    OutputStreamWriter outputStreamWriter = null;
 
-		try{
-			input = new FileInputStream("./src/files/files.txt");
-			ids = new InputStreamReader(input);
-			br = new BufferedReader(ids);
+    try {
+      // 在 idea 中就是 project(工作目录) 根目录+path
+      inputStream = new FileInputStream(from);
+      streamReader = new InputStreamReader(inputStream);
+      bufferedReader = new BufferedReader(streamReader);
 
+      outputStream = new FileOutputStream(dest);
+      outputStreamWriter = new OutputStreamWriter(outputStream);
+      bufferedWriter = new BufferedWriter(outputStreamWriter);
 
-			out = new FileOutputStream("./src/files/filess.txt");
-			os = new OutputStreamWriter(out);
-			bw = new BufferedWriter(os);
+      //刷新缓存到硬盘中
+      bufferedWriter.flush();
 
+      String L;
+      while ((L = bufferedReader.readLine()) != null) {
+        bufferedWriter.write(L + "\r\n");
+      }
+    } catch (Exception e) {
+      log.error(e.getMessage(), e);
+    } finally {
+      try {
+        // 先打开后关闭
+        ResourceTool.close(bufferedReader, streamReader, inputStream);
+        log.info("close all input stream");
 
+        ResourceTool.close(bufferedReader, outputStreamWriter, outputStream);
+        log.info("close all output stream");
+      } catch (Exception e) {
+        log.error(e.getMessage(), e);
+      }
+    }
+  }
 
-			bw.flush();//一定要清除缓存.
-
-			String L="";
-			while((L=br.readLine())!=null){
-				bw.write(L+"\r\n");
-			}
-		}catch(Exception e){
-			e.printStackTrace();
-			System.out.println("读取数据有异常");
-		}finally {
-			try {
-				//先打开，后关闭
-				input.close();
-				ids.close();
-				br.close();
-				System.out.println("全部关闭了输入流");
-
-			} catch (Exception e2) {
-				e2.printStackTrace();
-				System.out.println("有异常");
-			}
-
-			try {
-				//先打开，后关闭
-				bw.close();
-				os.close();
-				out.close();
-				System.out.println("保存数据并且关闭全部输出流");
-			}catch (Exception e2) {
-				e2.printStackTrace();
-				System.out.println("有异常");
-			}
-
-		}
-	}
+  // TODO 所有方式
 }
---
+++ java-io/src/test/java/com/github/kuangcp/inout/CopyFileTest.java
@@ -0,0 +1,61 @@
+package com.github.kuangcp.inout;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * created by https://gitee.com/gin9
+ *
+ * @author kuangcp on 3/17/19-12:34 AM
+ */
+@Slf4j
+public class CopyFileTest {
+
+  private CopyFile file = new CopyFile();
+
+  private String from = "src/test/resources/test.log";
+  private String dest = "src/test/resources/test2.log";
+  private Path fromPath = Paths.get(from);
+  private Path destPath = Paths.get(dest);
+
+  private String content = "hello world";
+
+  @Test
+  public void testFirst() throws IOException {
+    file.copyFileWithSixChannel(from, dest);
+    validate();
+  }
+
+  @Before
+  public void createFile() throws IOException {
+    Files.createFile(fromPath);
+    Files.write(fromPath, content.getBytes());
+    assert Files.exists(fromPath);
+  }
+
+  private void validate() throws IOException {
+    Optional<String> fileContent = Files.lines(fromPath).reduce(String::concat);
+    assert fileContent.isPresent();
+    log.debug("validate: content={}", fileContent.get());
+    assertThat(fileContent.get(), equalTo(content));
+  }
+
+  @After
+  public void deleteFile() throws IOException {
+    Files.delete(fromPath);
+    Files.delete(destPath);
+
+    assert !Files.exists(fromPath);
+    assert !Files.exists(destPath);
+  }
+}
