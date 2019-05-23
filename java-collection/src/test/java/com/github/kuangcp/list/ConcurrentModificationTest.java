d189e2c7a3cff9be1e89c52bc05509fc6dac96eb
Kuangcp <kuangcp@aliyun.com>
2019-05-23 23:22:58 +0800

    +) remove from list

--- java-collection/src/test/java/com/github/kuangcp/list/ConcurrentModificationTest.java
+++ java-collection/src/test/java/com/github/kuangcp/list/ConcurrentModificationTest.java
@@ -1,6 +1,10 @@
 package com.github.kuangcp.list;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 
 /**
  * TODO list 发生 并发修改异常时的处理方式
@@ -10,4 +14,36 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ConcurrentModificationTest {
 
+  private List<String> list = new ArrayList<>(10);
+
+  @Test
+  public void testGenerate() {
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i).equals("del")) {
+        list.remove(i);
+      }
+    }
+  }
+
+  @Test
+  public void testForEach() {
+    for (String x : list) {
+      if (x.equals("del")) {
+        list.remove(x);
+      }
+    }
+  }
+  
+  @Test
+  public void testIterator(){
+    list.removeIf(x -> x.equals("del"));
+
+    Iterator<String> it = list.iterator();
+    while(it.hasNext()){
+      String x = it.next();
+      if(x.equals("del")){
+        it.remove();
+      }
+    }
+  }
 }
