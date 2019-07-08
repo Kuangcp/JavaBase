b5b1e450f9ee5c9526555cb858741e4779f485db
kcp <kuangcp@aliyun.com>
2019-07-08 17:02:49 +0800

    x) extract common cofig

--- collection/src/test/java/com/github/kuangcp/map/HashMapTest.java
+++ collection/src/test/java/com/github/kuangcp/map/HashMapTest.java
@@ -1,23 +1,30 @@
 package com.github.kuangcp.map;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 /**
  * @author kuangcp on 2019-04-16 10:22 AM
  */
+@Slf4j
 public class HashMapTest {
 
   @Builder
   @NoArgsConstructor
   @AllArgsConstructor
-  static class Foo {
+  static class Banana {
 
-    private String name;
+    private String tag;
 
     @Override
     public int hashCode() {
@@ -26,7 +33,7 @@ public class HashMapTest {
 
     @Override
     public String toString() {
-      return name;
+      return tag;
     }
   }
 
@@ -35,16 +42,57 @@ public class HashMapTest {
    */
   @Test
   public void testSameHashCode() {
-    Map<Foo, Integer> map = new HashMap<>();
+    Map<Banana, Integer> map = new HashMap<>();
     for (int i = 0; i < 10; i++) {
-      map.put(new Foo(), 1);
+      map.put(new Banana(), 1);
     }
 
-    Foo you = Foo.builder().name("you").build();
-    map.put(you, 12);
-    map.remove(new Foo());
+    Banana banana = Banana.builder().tag("banana").build();
+    map.put(banana, 12);
+    map.remove(new Banana());
 
     System.out.println("size=" + map.size());
     map.keySet().forEach(System.out::println);
+  }
+
+  /**
+   * 注意 keySet 返回的是 Map 中的存储结构, 如果改变 keySet, 就会影响到原有的Map
+   */
+  @Test
+  public void testKeySet() {
+    Map<String, String> dict1 = new HashMap<>();
+    Map<String, String> dict2 = new HashMap<>();
+
+    dict1.put("1", "5");
+    dict1.put("2", "5");
+    dict1.put("3", "5");
+
+    dict2.put("4", "5");
+    dict2.put("3", "5");
+    dict2.put("2", "5");
+
+    log.info("new set");
+
+    HashSet<String> common = new HashSet<>(dict1.keySet());
+    common.retainAll(dict2.keySet());
+    assertThat(common.size(), equalTo(2));
+    assertThat(dict1.size(), equalTo(3));
+    assertThat(dict2.size(), equalTo(3));
+    showMap(dict1);
+
+    log.info("origin set");
+    dict1.keySet().retainAll(dict2.keySet());
+    assertThat(dict1.size(), equalTo(2));
+    assertThat(dict2.size(), equalTo(3));
+
+    showMap(dict1);
+  }
+
+  private <K, V> void showMap(Map<K, V> map) {
+    if (Objects.isNull(map)) {
+      return;
+    }
+
+    map.forEach((k, v) -> log.info("k={} v={}", k, v));
   }
 }
