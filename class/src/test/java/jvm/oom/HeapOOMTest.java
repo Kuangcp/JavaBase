9468aaf2c65a96211bf95851c3f26f6702e3a639
Kuangcp <kuangcp@aliyun.com>
2019-07-14 17:24:45 +0800

    *) simplify Heap OOM

--- class/src/test/java/jvm/oom/HeapOOMTest.java
+++ class/src/test/java/jvm/oom/HeapOOMTest.java
@@ -1,14 +1,11 @@
 package jvm.oom;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Ignore;
 import org.junit.Test;
 
 /**
- * -Xms5m -Xmx5m
+ * -Xms30m -Xmx30m -XX:+PrintGCDetails
  *
  * @author kuangcp on 4/5/19-9:45 AM
  */
@@ -19,50 +16,22 @@ public class HeapOOMTest {
   private HeapOOM heapOOM = new HeapOOM();
 
   @Test
-  public void testOOMWithOtherThread() throws Exception {
-    // 如果将该集合放在这里, 那么就不会被回收, 导致OOM 所有线程都玩完
-    // List<byte[]> data = new ArrayList<>();
-
-    Thread main = new Thread(() -> {
-      List<byte[]> data = new ArrayList<>();
-      while (true) {
-        // 因为当这里无法分配内存 OOM 后就出循环了, 对象也就可以被回收了
-        data.add(new byte[1024 * 256]);
-        try {
-          TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-        log.info("allocate memory");
-      }
-    });
-
-    Thread thread = new Thread(() -> {
-      while (true) {
-        try {
-          TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-        log.info("timer");
-      }
-    });
-
-    main.start();
-    thread.start();
-
-    main.join();
-    thread.join();
-  }
-
-  @Test
   public void testCreateArray() {
     heapOOM.createArray();
   }
 
   @Test
-  @Ignore
-  public void testCreateMap() throws Exception {
+  public void testCreateMap() {
     heapOOM.createMap();
+  }
+
+  @Test
+  public void testCreateWeakMap() {
+    heapOOM.createWeakMap();
+  }
+
+  @Test
+  public void testOtherThreadWithOOM() throws Exception {
+    heapOOM.otherThreadWithOOM();
   }
 }
