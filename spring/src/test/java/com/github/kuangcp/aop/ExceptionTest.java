d5360db085efd74b9c30edef002e7074f9221e1e
kcp <kuangcp@aliyun.com>
2019-07-26 10:04:49 +0800

    x) use correct path

--- spring/src/test/java/com/github/kuangcp/aop/ExceptionTest.java
+++ spring/src/test/java/com/github/kuangcp/aop/ExceptionTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class ExceptionTest extends SpringHelper {
 
   static {
-    path = "applicationContext-exception.xml";
+    path = "aop/exception/applicationContext-exception.xml";
   }
 
   @Test
