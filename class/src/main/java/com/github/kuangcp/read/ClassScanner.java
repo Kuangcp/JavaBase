d5460497ab6f58cf128c5c0d1ff986b8a8e1a585
Kuangcp <kuangcp@aliyun.com>
2019-09-01 00:33:35 +0800

    *) use log rather than sout

--- class/src/main/java/com/github/kuangcp/read/ClassScanner.java
+++ class/src/main/java/com/github/kuangcp/read/ClassScanner.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Enumeration;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -14,14 +15,18 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.regex.Pattern;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Created by https://github.com/kuangcp 读取类  是一个工具类
  *
  * @author kuangcp
  */
+@Slf4j
 @NoArgsConstructor
 public class ClassScanner {
+
+  private static final String suffix = ".class";
 
   private boolean checkInOrEx;
   private List<String> classFilters;
@@ -50,6 +55,7 @@ public class ClassScanner {
     if (basePackage.endsWith(".")) {
       packageName = basePackage.substring(0, basePackage.length() - 1);
     }
+
     String package2Path = packageName.replace('.', '/');
     try {
       Enumeration<URL> dirs = Thread.currentThread().getContextClassLoader()
@@ -58,15 +64,15 @@ public class ClassScanner {
         URL url = dirs.nextElement();
         String protocol = url.getProtocol();
         if ("file".equals(protocol)) {
-          String filePath = URLDecoder.decode(url.getFile(), "UTF-8");
+          String filePath = URLDecoder.decode(url.getFile(), StandardCharsets.UTF_8.name());
           this.doScanPackageClassesByFile(classes, packageName, filePath, recursive);
         } else if ("jar".equals(protocol)) {
           this.doScanPackageClassesByJar(packageName, url, recursive, classes);
         }
       }
       return classes;
-    } catch (IOException var10) {
-      var10.printStackTrace();
+    } catch (IOException e) {
+      log.error(e.getMessage(), e);
       throw new InternalError("扫描包失败");
     }
   }
@@ -74,37 +80,45 @@ public class ClassScanner {
   /**
    * 从文件中读取类
    */
-  private void doScanPackageClassesByFile(Set<Class<?>> classes, String packageName,
-      String packagePath, final boolean recursive) {
+  private void doScanPackageClassesByFile(
+      Set<Class<?>> classes,
+      String packageName,
+      String packagePath,
+      final boolean recursive) {
+
     File dir = new File(packagePath);
-    if (dir.exists() && dir.isDirectory()) {
-      File[] files = dir.listFiles(file -> {
-        if (file.isDirectory()) {
-          return recursive;
-        } else {
-          String filename = file.getName();
-          return (!ClassScanner.this.excludeInner || filename.indexOf(36) == -1)
-              && ClassScanner.this.filterClassName(filename);
-        }
-      });
-      if (Objects.isNull(files)) {
-        return;
+    if (!dir.exists() || !dir.isDirectory()) {
+      return;
+    }
+
+    File[] files = dir.listFiles(file -> {
+      if (file.isDirectory()) {
+        return recursive;
+      } else {
+        String filename = file.getName();
+        return (!ClassScanner.this.excludeInner || filename.indexOf(36) == -1)
+            && ClassScanner.this.filterClassName(filename);
       }
+    });
 
-      for (File file : files) {
-        if (file.isDirectory()) {
-          this.doScanPackageClassesByFile(classes, packageName + "." + file.getName(),
-              file.getAbsolutePath(), recursive);
-        } else {
-          String className = file.getName().substring(0, file.getName().length() - 6);
+    if (Objects.isNull(files)) {
+      return;
+    }
 
-          try {
-            classes.add(Thread.currentThread().getContextClassLoader()
-                .loadClass(packageName + '.' + className));
-          } catch (ClassNotFoundException var14) {
-            var14.printStackTrace();
-            throw new InternalError("过滤失败");
-          }
+    for (File file : files) {
+      if (file.isDirectory()) {
+        this.doScanPackageClassesByFile(classes, packageName + "." + file.getName(),
+            file.getAbsolutePath(), recursive);
+      } else {
+        String className = file.getName().substring(0, file.getName().length() - suffix.length());
+
+        try {
+          String classPath = packageName + '.' + className;
+          Class<?> target = Thread.currentThread().getContextClassLoader().loadClass(classPath);
+          classes.add(target);
+        } catch (ClassNotFoundException e) {
+          log.error(e.getMessage(), e);
+          throw new InternalError("过滤失败");
         }
       }
     }
@@ -145,24 +159,26 @@ public class ClassScanner {
         String classSimpleName = name.substring(name.lastIndexOf(47) + 1);
         if (this.filterClassName(classSimpleName)) {
           String className = name.replace('/', '.');
-          className = className.substring(0, className.length() - 6);
+          className = className.substring(0, className.length() - suffix.length());
 
           try {
             classes.add(Thread.currentThread().getContextClassLoader().loadClass(className));
-          } catch (ClassNotFoundException var14) {
-            var14.printStackTrace();
-            throw new InternalError("过滤失败");
+          } catch (ClassNotFoundException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException("过滤失败");
           }
         }
       }
+
     } catch (IOException e) {
-      e.printStackTrace();
+      log.error(e.getMessage(), e);
       throw new InternalError("扫描jar失败");
     }
   }
 
   private boolean filterClassName(String className) {
-    if (!className.endsWith(".class")) {
+
+    if (!className.endsWith(suffix)) {
       return false;
     }
 
@@ -171,16 +187,13 @@ public class ClassScanner {
     }
 
     String tmpName = className.substring(0, className.length() - 6);
-    boolean flag = false;
+    boolean matchFlag = this.classFilters.stream()
+        .anyMatch(v -> {
+          String reg = "^" + v.replace("*", ".*") + "$";
+          Pattern p = Pattern.compile(reg);
+          return p.matcher(tmpName).find();
+        });
 
-    for (String str : this.classFilters) {
-      String tmpreg = "^" + str.replace("*", ".*") + "$";
-      Pattern p = Pattern.compile(tmpreg);
-      if (p.matcher(tmpName).find()) {
-        flag = true;
-        break;
-      }
-    }
-    return this.checkInOrEx && flag || !this.checkInOrEx && !flag;
+    return this.checkInOrEx && matchFlag || !this.checkInOrEx && !matchFlag;
   }
 }
