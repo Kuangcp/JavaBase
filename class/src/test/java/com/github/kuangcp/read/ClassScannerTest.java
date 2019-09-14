c737f5ad9c8962738e2a86db1e40e3158a6d3d81
Kuangcp <kuangcp@aliyun.com>
2019-09-14 09:57:59 +0800

    +) test jar

--- class/src/test/java/com/github/kuangcp/read/ClassScannerTest.java
+++ class/src/test/java/com/github/kuangcp/read/ClassScannerTest.java
@@ -1,6 +1,8 @@
 package com.github.kuangcp.read;
 
+import com.github.kuangcp.time.GetRunTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
@@ -16,15 +18,18 @@ import org.junit.Test;
 public class ClassScannerTest {
 
   @Test
-  public void testGetPackageAllClasses() throws Exception {
+  public void testGetPackageAllClasses() {
     // 递归找到当前类所在包的所有 Base 后缀且继承了抽象逻辑类, 然后生成代码
     List<String> list = new ArrayList<>();
     list.add("*Base");
     ClassScanner scanner = new ClassScanner(true, true, list);
+
     System.out.println(ClassScannerTest.class.getPackage().getName());
     Set<Class<?>> clazzSet = scanner
         .getPackageAllClasses(ClassScannerTest.class.getPackage().getName(), true);
+
     log.info("size: {}", clazzSet.size());
+
     for (Class<?> clazz : clazzSet) {
       if (clazz != BaseFather.class && BaseFather.class.isAssignableFrom(clazz)) { // 继承自 BaseFather
         String rawName = clazz.getSimpleName();
@@ -33,6 +38,15 @@ public class ClassScannerTest {
             ("    public final static %s %s = new %s();", rawName, name, rawName));
       }
     }
+  }
+
+  @Test
+  public void testReadJar(){
+    ClassScanner scanner = new ClassScanner(true, true, Collections.emptyList());
+    Set<Class<?>> result = scanner.getPackageAllClasses(GetRunTime.class.getPackage().getName(), true);
+    log.info("result {}", result.size());
+
+    result.forEach(System.out::println);
   }
 
   private static String lowerCaseFirstLetter(String name) {
