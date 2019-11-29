e6b51464ca79dd4f21097c1915d7b38a334246b4
kcp <kuangcp@aliyun.com>
2019-11-29 14:19:40 +0800

    +) filter and &&

--- java8/src/test/java/com/github/kuangcp/stream/filter/FilterTest.java
+++ java8/src/test/java/com/github/kuangcp/stream/filter/FilterTest.java
@@ -1,8 +1,14 @@
 package com.github.kuangcp.stream.filter;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.IntStream;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author kuangcp on 3/6/19-5:08 PM
@@ -25,5 +31,16 @@ public class FilterTest {
   public void testFilterWithNull() {
     long count = IntStream.rangeClosed(1, 10).filter(this::isTrue).count();
     System.out.println(count);
+  }
+
+  // 多个 filter 具有 && 一样的短路效应
+  @Test
+  public void testAnd() {
+    List<String> data = Arrays.asList("", null, "2", "12");
+    long count = data.stream()
+        .filter(Objects::nonNull)
+        .filter(v -> v.length() > 1)
+        .count();
+    assertThat(count, equalTo(1L));
   }
 }
