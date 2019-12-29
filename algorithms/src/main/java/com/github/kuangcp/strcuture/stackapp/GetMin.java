357c928e298ab927daff8b7d3ccbd28fecb470f5
Kuangcp <kuangcp@aliyun.com>
2019-12-29 23:58:08 +0800

    +) min

---
+++ algorithms/src/main/java/com/github/kuangcp/strcuture/stackapp/GetMin.java
@@ -0,0 +1,63 @@
+package com.github.kuangcp.strcuture.stackapp;
+
+import java.util.Objects;
+import java.util.Stack;
+import lombok.Data;
+
+/**
+ * @author https://github.com/kuangcp on 2019-12-29 16:45
+ */
+@Data
+public class GetMin {
+
+  private Stack<Integer> data = new Stack<>();
+  private Stack<Integer> min = new Stack<>();
+
+  void pushA(int value) {
+    data.push(value);
+    if (min.isEmpty()) {
+      min.push(value);
+    } else if (min.peek() >= value) {
+      min.push(value);
+    }
+  }
+
+  int popA() {
+    if (data.isEmpty()) {
+      return -1;
+    }
+
+    Integer value = data.pop();
+    if (!min.isEmpty() && Objects.equals(value, min.peek())) {
+      min.pop();
+    }
+    return value;
+  }
+
+  int getMin() {
+    return min.peek();
+  }
+
+  void pushB(int value) {
+    data.push(value);
+    if (min.isEmpty()) {
+      min.push(value);
+    } else if (min.peek() >= value) {
+      min.push(value);
+    } else {
+      min.push(min.peek());
+    }
+  }
+
+  int popB() {
+    if (!min.isEmpty()) {
+      min.pop();
+    }
+
+    if (!data.isEmpty()) {
+      return data.pop();
+    }
+
+    return -1;
+  }
+}
---
+++ algorithms/src/test/java/com/github/kuangcp/strcuture/stackapp/GetMinTest.java
@@ -0,0 +1,37 @@
+package com.github.kuangcp.strcuture.stackapp;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+/**
+ * @author https://github.com/kuangcp on 2019-12-29 23:45
+ */
+public class GetMinTest {
+
+
+  @Test
+  public void testA() throws Exception {
+    GetMin app = new GetMin();
+    app.pushA(2);
+    app.pushA(0);
+    app.pushA(6);
+    app.pushA(10);
+    app.pushA(1);
+    app.pushA(0);
+    app.popA();
+
+    assertThat(app.getMin(), equalTo(0));
+  }
+
+  @Test
+  public void testB() throws Exception {
+    GetMin app = new GetMin();
+    app.pushB(3);
+    app.pushB(2);
+    app.pushB(9);
+    app.popB();
+    assertThat(app.getMin(), equalTo(2));
+  }
+}
