73679e0254cb50ec26756eeeacc0b16f2ddca9dd
Kuangcp <kuangcp@aliyun.com>
2021-05-18 08:57:58 +0800

    +) active count

--- netty/src/main/java/netty/websocket/ChannelSupervise.java
+++ netty/src/main/java/netty/websocket/ChannelSupervise.java
@@ -8,30 +8,39 @@ import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.util.concurrent.GlobalEventExecutor;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author https://github.com/kuangcp on 2021-05-18 08:34
  */
+@Slf4j
 public class ChannelSupervise {
 
-  private static final ChannelGroup GlobalGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
-  private static final ConcurrentMap<String, ChannelId> ChannelMap = new ConcurrentHashMap<>();
+  private static final ChannelGroup GLOBAL_GROUP = new DefaultChannelGroup(
+      GlobalEventExecutor.INSTANCE);
+  private static final ConcurrentMap<String, ChannelId> CHANNEL_MAP = new ConcurrentHashMap<>();
 
   public static void addChannel(Channel channel) {
-    GlobalGroup.add(channel);
-    ChannelMap.put(channel.id().asShortText(), channel.id());
+    GLOBAL_GROUP.add(channel);
+    CHANNEL_MAP.put(channel.id().asShortText(), channel.id());
+    printState();
   }
 
   public static void removeChannel(Channel channel) {
-    GlobalGroup.remove(channel);
-    ChannelMap.remove(channel.id().asShortText());
+    GLOBAL_GROUP.remove(channel);
+    CHANNEL_MAP.remove(channel.id().asShortText());
+    printState();
   }
 
   public static Channel findChannel(String id) {
-    return GlobalGroup.find(ChannelMap.get(id));
+    return GLOBAL_GROUP.find(CHANNEL_MAP.get(id));
   }
 
   public static void send2All(TextWebSocketFrame tws) {
-    GlobalGroup.writeAndFlush(tws);
+    GLOBAL_GROUP.writeAndFlush(tws);
+  }
+
+  private static void printState() {
+    log.info("channelSize={} global={}", CHANNEL_MAP.size(), GLOBAL_GROUP.size());
   }
 }
