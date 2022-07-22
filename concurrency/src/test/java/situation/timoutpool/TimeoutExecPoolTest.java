31af4416e7688261af29e986c7e0af9f1c5a44bc
kcp <kuangcp@aliyun.com>
2022-07-22 14:53:42 +0800

    feat: timeout task exec control

--- concurrency/src/test/java/situation/timoutpool/TimeoutExecPoolTest.java
+++ concurrency/src/test/java/situation/timoutpool/TimeoutExecPoolTest.java
@@ -2,11 +2,11 @@ package situation.timoutpool;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
-import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -29,14 +29,105 @@ class TimeoutExecPoolTest {
 //        Thread.currentThread().join();
     }
 
+    /**
+     * 可行方案： 使用 ExecutorCompletionService 和 CompletableFuture 组合限制执行时间，汇聚执行结果，取消后续任务
+     */
+    @Test
+    public void testCompleteService() {
+        log.info("start");
+        CompletionService<Long> cs = new ExecutorCompletionService<>(Executors.newFixedThreadPool(3));
+
+        int max = 10;
+        List<String> params = IntStream.range(0, max)
+                .mapToObj(v -> UUID.randomUUID().toString() + v)
+                .collect(Collectors.toList());
+        List<Future<Long>> futures = params.stream().map(v -> cs.submit(() -> this.logic(v))).collect(Collectors.toList());
+
+        CompletableFuture<List<Long>> future = CompletableFuture.supplyAsync(() -> {
+            List<Long> results = new ArrayList<>();
+            for (int i = 0; i < max; i++) {
+                Future<Long> take;
+                try {
+                    take = cs.take();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                Long result;
+                try {
+                    result = take.get();
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+                log.info("{}", result);
+                results.add(result);
+            }
+            return results;
+        });
+        try {
+            List<Long> results = future.get(80000, TimeUnit.MILLISECONDS);
+            log.info("{}", results);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            log.error("timeout", e);
+            for (Future<Long> longFuture : futures) {
+                longFuture.cancel(true);
+            }
+        }
+    }
+
+    /**
+     * 保留部分任务执行数据 中断后续任务
+     */
+    @Test
+    public void testCompleteServicePart() {
+        log.info("start");
+        CompletionService<Long> cs = new ExecutorCompletionService<>(Executors.newFixedThreadPool(2));
+
+        int max = 10;
+        List<String> params = IntStream.range(0, max)
+                .mapToObj(v -> UUID.randomUUID().toString() + v)
+                .collect(Collectors.toList());
+        List<Future<Long>> futures = params.stream().map(v -> cs.submit(() -> this.logic(v))).collect(Collectors.toList());
+
+        Vector<Long> vs = new Vector<>();
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+            for (int i = 0; i < max; i++) {
+                Future<Long> take;
+                try {
+                    take = cs.take();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                Long result;
+                try {
+                    result = take.get();
+                    vs.add(result);
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+                log.info("{}", result);
+            }
+        });
+        log.info("wait result");
+        try {
+            future.get(800, TimeUnit.MILLISECONDS);
+            log.info("{}", vs);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            log.error("timeout", e);
+            log.warn("part： {}", vs);
+            for (Future<Long> longFuture : futures) {
+                longFuture.cancel(true);
+            }
+        }
+    }
+
     private Long logic(String param) {
         if (Objects.isNull(param)) {
             return 0L;
         }
         try {
-            Thread.sleep(150);
+            Thread.sleep(350);
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            log.error("", e);
         }
         return (long) param.length();
     }
