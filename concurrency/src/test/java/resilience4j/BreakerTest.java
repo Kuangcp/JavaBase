87a46d4ea44304b814826d244c008cca2a1c40c7
kcp <kuangcp@aliyun.com>
2022-08-03 19:30:30 +0800

    mod: Resilience4j

---
+++ concurrency/src/test/java/resilience4j/BreakerTest.java
@@ -0,0 +1,46 @@
+package resilience4j;
+
+
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.vavr.control.Try;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.function.Supplier;
+
+
+/**
+ * @author KuangChengPing@51peiq.com on 2022-08-03 19:20
+ */
+public class BreakerTest {
+
+    private String doAction() {
+        return "true";
+    }
+
+    @Test
+    public void testBreaker() {
+
+// Create a custom RateLimiter configuration
+        RateLimiterConfig config = RateLimiterConfig.custom()
+                .timeoutDuration(Duration.ofMillis(100))
+                .limitRefreshPeriod(Duration.ofSeconds(1))
+                .limitForPeriod(1)
+                .build();
+// Create a RateLimiter
+        RateLimiter rateLimiter = RateLimiter.of("backendName", config);
+
+// Decorate your call to BackendService.doSomething()
+        Supplier<String> restrictedSupplier = RateLimiter
+                .decorateSupplier(rateLimiter, this::doAction);
+
+// First call is successful
+        Try<String> firstTry = Try.ofSupplier(restrictedSupplier);
+        assert firstTry.isSuccess();
+// Second call fails, because the call was not permitted
+//        Try<String> secondTry = Try.of(restrictedSupplier);
+//        assert secondTry.isFailure();
+//        assertThat(secondTry.getCause()).isInstanceOf(RequestNotPermitted.class);
+    }
+}
