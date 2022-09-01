2205d3d0c89648ed137afcfb907f871f7d3d72b8
Kuangcp <kuangcp@aliyun.com>
2022-09-02 00:34:46 +0800

    rm: other mod

--- flink/run.sh
+++
@@ -1,5 +0,0 @@
-. ./host.conf
-jarId=$1
-
-curl -X POST $host/jars/$jarId/run\?entry-class\=com.github.kuangcp.hi.SimpleStatistic
-
--- flink/upload.sh
+++
@@ -1,6 +0,0 @@
-. ./host.conf
-file=$(find . -iname "*.jar*")
-echo $file
-
-curl -X POST -H "Expect:" -F "jarfile=@$file" $host/jars/upload
-
