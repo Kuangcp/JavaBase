55625d27f119c5d91267ede5af778abde36f2acd
kuangcp <kuangcp@aliyun.com>
2024-01-24 18:29:59 +0800

    demo

--- concurrency/src/test/java/com/github/kuangcp/latch/CountDownLatchTest.java
+++ concurrency/src/test/java/com/github/kuangcp/latch/CountDownLatchTest.java
@@ -3,10 +3,7 @@ package com.github.kuangcp.latch;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @author https://github.com/kuangcp on 2021-09-04 23:02
@@ -33,5 +30,26 @@ public class CountDownLatchTest {
         }
 
         latch.await(3, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testSemaphore() throws Exception {
+        final ExecutorService pool = Executors.newFixedThreadPool(10);
+        Semaphore semaphore = new Semaphore(3, true);
+        for (int i = 0; i < 30; i++) {
+            pool.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (Exception e) {
+                    log.error("", e);
+                } finally {
+                    semaphore.release();
+                }
+                log.info("run");
+            });
+        }
+
+        Thread.currentThread().join();
     }
 }
