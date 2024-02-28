07f3cb8b332c4cae7bdc6fad7e64e07f50647723
kuangcp <kuangcp@aliyun.com>
2024-02-28 20:23:31 +0800

    netty websocket url param

--- netty/src/main/java/netty/websocket/NioWebSocketChannelInitializer.java
+++ netty/src/main/java/netty/websocket/NioWebSocketChannelInitializer.java
@@ -1,22 +1,29 @@
 package netty.websocket;
 
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
+
+import static io.netty.handler.codec.http.HttpHeaders.Names.WEBSOCKET_PROTOCOL;
 
 /**
  * @author https://github.com/kuangcp on 2021-05-18 08:33
  */
 public class NioWebSocketChannelInitializer extends ChannelInitializer<SocketChannel> {
-  @Override
-  protected void initChannel(SocketChannel ch) {
-    ch.pipeline().addLast("logging",new LoggingHandler("DEBUG"));//设置log监听器，并且日志级别为debug，方便观察运行流程
-    ch.pipeline().addLast("http-codec",new HttpServerCodec());//设置解码器
-    ch.pipeline().addLast("aggregator",new HttpObjectAggregator(65536));//聚合器，使用websocket会用到
-    ch.pipeline().addLast("http-chunked",new ChunkedWriteHandler());//用于大数据的分区传输
-    ch.pipeline().addLast("handler",new NioWebSocketHandler());//自定义的业务handler
-  }
+    @Override
+    protected void initChannel(SocketChannel ch) {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast("logging", new LoggingHandler("DEBUG"));//设置log监听器，并且日志级别为debug，方便观察运行流程
+        pipeline.addLast("http-codec", new HttpServerCodec());//设置解码器
+        pipeline.addLast("aggregator", new HttpObjectAggregator(65536));//聚合器，使用websocket会用到
+        pipeline.addLast("http-chunked", new ChunkedWriteHandler());//用于大数据的分区传输
+        pipeline.addLast("handler", new NioWebSocketHandler());//自定义的业务handler
+        // checkStartsWith 为true 支持路径带参数
+        pipeline.addLast("", new WebSocketServerProtocolHandler(Const.webSocketPath, WEBSOCKET_PROTOCOL, true, 65536, false, true));
+    }
 }
--- netty/src/main/java/netty/websocket/Readme.md
+++ netty/src/main/java/netty/websocket/Readme.md
@@ -1,3 +1,5 @@
-Netty 实现的 websocket 
+Netty 实现的 websocket
 
-TODO 对比 Tomcat，Spring实现 进行性能测试(固定 JVM 参数 相同硬件，测试最大连接数，失败数，GC情况，延迟)
+[SpringBoot整合Netty处理WebSocket（支持url参数）](https://blog.csdn.net/RisenMyth/article/details/104441155)
+
+TODO 对比 Tomcat实现 进行性能测试(固定 JVM 参数 相同硬件，测试最大连接数，失败数，GC情况，延迟)
