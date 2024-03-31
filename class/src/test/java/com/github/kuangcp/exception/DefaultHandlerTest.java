c5a210d5faf172e30681be4134884fe8481b3bb7
Kuangcp <kuangcp@aliyun.com>
2024-04-01 00:49:42 +0800

    compare handle exception

--- class/src/test/java/com/github/kuangcp/exception/DefaultHandlerTest.java
+++ class/src/test/java/com/github/kuangcp/exception/DefaultHandlerTest.java
@@ -13,47 +13,49 @@ public class DefaultHandlerTest {
     @Test
     public void testDefaultHandler() throws Exception {
         // TODO not work ?
-        Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
-            log.error("", e);
-            System.out.println(e.getMessage().length());
-        });
-        System.out.println(1 / 0);
-        System.out.println("handle");
+        log.info("start");
+//        TimeUnit.MINUTES.sleep(10);
+        setUp();
+        hitDefault();
     }
 
     public static void main(String[] args) {
-        try {
-            hitDefault();
-        } catch (Exception e) {
-            log.error("", e);
-        }
-
-        try {
-            notHitDefault();
-        } catch (Exception e) {
-            log.error("", e);
-        }
+        setUp();
+        notHitDefault();
+        hitDefault2();
+//        hitDefault();
     }
 
-    private static void hitDefault() {
-        System.out.println(Thread.currentThread().getName());
+    private static void setUp(){
         Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
-            log.error("", e);
+            log.error("DEFAULT", e);
             System.out.println(e.getMessage().length());
         });
+    }
+    private static void hitDefault() {
+        System.out.println(">>> Thread "+Thread.currentThread().getName());
         System.out.println(1 / 0);
         System.out.println("handle");
     }
-
-    private static void notHitDefault() {
-        System.out.println(Thread.currentThread().getName());
-        Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
-            log.error("", e);
-            System.out.println(e.getMessage().length());
-        });
+    private static void hitDefault2() {
+        System.out.println(">>> hitDefault2 "+Thread.currentThread().getName());
         try {
             System.out.println(1 / 0);
         } catch (Exception e) {
+            log.error("", e);
+            throw e;
+        }
+
+        System.out.println("handle");
+    }
+
+
+    private static void notHitDefault() {
+        System.out.println(">>> Thread "+Thread.currentThread().getName());
+        try {
+            System.out.println(1 / 0);
+        } catch (Exception e) {
+            // 异常已经被此处声明的 handler 捕获，不会进入上述声明的 默认handler
             log.error("", e);
         }
         System.out.println("handle");
