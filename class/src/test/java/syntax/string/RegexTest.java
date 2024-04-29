2f1dcd0740709602ba5010bd78856f98569f3b07
kuangcp <kuangcp@aliyun.com>
2024-04-29 11:16:45 +0800

    compare with pool

--- class/src/test/java/syntax/string/RegexTest.java
+++ class/src/test/java/syntax/string/RegexTest.java
@@ -1,8 +1,12 @@
 package syntax.string;
 
 import org.junit.Test;
+import syntax.bit.BitOperatorsTest;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * @author Kuangcp
@@ -14,13 +18,56 @@ public class RegexTest {
     @Test
     public void testCompareCache() throws Exception {
         for (int i = 0; i < 100; i++) {
-            compare();
+            compare(10_0000);
         }
     }
 
-    private static void compare() {
+    @Test
+    public void testJoinPoolOpt() throws Exception {
+        for (int i = 0; i < 300; i++) {
+            compareByPool(100_0000);
+        }
+    }
+
+    private static void compareByPool(int scale) {
         long start = System.currentTimeMillis();
-        for (int i = 0; i < 100000; i++) {
+
+        IntStream.range(0, scale).parallel().forEach(v -> {
+            Pattern pt = Pattern.compile("（.*?）|\\(.*?\\)");
+            pt.matcher("category");
+            pt.matcher("category(who)（name）");
+        });
+
+        long end = System.currentTimeMillis();
+        long no = end - start;
+
+        start = System.currentTimeMillis();
+        Pattern pt = Pattern.compile("（.*?）|\\(.*?\\)");
+
+        IntStream.range(0, scale).parallel().forEach(v -> {
+            pt.matcher("category");
+            pt.matcher("category(who)（name）");
+        });
+        end = System.currentTimeMillis();
+        long cac = end - start;
+
+        start = System.currentTimeMillis();
+
+        IntStream.range(0, scale).parallel().forEach(v -> {
+            hasBracket("category");
+            hasBracket("category(who)（name）");
+        });
+        end = System.currentTimeMillis();
+        long loop = end - start;
+        System.out.println(no + " " + cac + " " + loop);
+    }
+
+    /**
+     * https://www.jianshu.com/p/f313746119ad
+     */
+    private static void compare(int scale) {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < scale; i++) {
             Pattern pt = Pattern.compile("（.*?）|\\(.*?\\)");
             pt.matcher("category");
             pt.matcher("category(who)（name）");
@@ -30,12 +77,49 @@ public class RegexTest {
 
         start = System.currentTimeMillis();
         Pattern pt = Pattern.compile("（.*?）|\\(.*?\\)");
-        for (int i = 0; i < 100000; i++) {
-            pt.matcher("xxxx");
-            pt.matcher("xxxx(who)");
+        for (int i = 0; i < scale; i++) {
+            pt.matcher("category");
+            pt.matcher("category(who)（name）");
         }
         end = System.currentTimeMillis();
         long cac = end - start;
         System.out.println(no + " " + cac);
+    }
+
+    /**
+     * TODO 比缓存的正则慢
+     *
+     * @see BitOperatorsTest
+     */
+    private static boolean hasBracket(String msg) {
+        if (Objects.isNull(msg)) {
+            return false;
+        }
+        int res = 0b0;
+        for (int i = 0; i < msg.length(); i++) {
+            if (Objects.equals(msg.charAt(i), '（')) {
+                res = res | 0b1;
+            } else if (Objects.equals(msg.charAt(i), '）')) {
+                res = res | 0b10;
+            } else if (Objects.equals(msg.charAt(i), '(')) {
+                res = res | 0b100;
+            } else if (Objects.equals(msg.charAt(i), ')')) {
+                res = res | 0b1000;
+            }
+        }
+//        int a = res & 0b11;
+//        int b = res & 0b1100;
+//        System.out.println(a + " " + b);
+//        System.out.println(a == 0b11);
+//        System.out.println(b == 0b1100);
+        return (res & 0b11) == 0b11 || (res & 0b1100) == 0b1100;
+    }
+
+    @Test
+    public void testBracket() throws Exception {
+        System.out.println(hasBracket("a(sss）"));
+        System.out.println(hasBracket("a(sss)"));
+        System.out.println(hasBracket("a（sss）"));
+        System.out.println(hasBracket("a(sss（"));
     }
 }
