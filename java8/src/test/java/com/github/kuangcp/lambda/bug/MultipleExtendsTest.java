379552d306209e7a5d8dbfc3b56552a7eaebf9a8
Kuangcp <kuangcp@aliyun.com>
2024-07-10 00:37:52 +0800

    bugs

--- java8/src/test/java/com/github/kuangcp/lambda/bug/MultipleExtendsTest.java
+++ java8/src/test/java/com/github/kuangcp/lambda/bug/MultipleExtendsTest.java
@@ -1,45 +1,47 @@
 package com.github.kuangcp.lambda.bug;
 
+import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.junit.Test;
 
 /**
  * @author <a href="https://github.com/kuangcp">Kuangcp</a> on 2019-11-28 19:43
  */
 public class MultipleExtendsTest {
 
-  @Test
-  public void testMap() {
-    List<C> list = Arrays.asList(new CImpl(), new CImpl());
-    List<String> names = list.stream().map(A::getName).collect(Collectors.toList());
-    System.out.println(names);
-  }
+    // 普通写法
+    @Test
+    public void testMap() {
+        List<C> list = Arrays.asList(new CImpl(), new CImpl());
+        List<String> names = list.stream().map(A::getName).collect(Collectors.toList());
+        System.out.println(names);
+    }
 
-  private static <E extends A & B> void getNames(List<E> list) {
-    list.stream().map(E::getName).count();
-    list.stream().map(E::getNum).count();
-  }
+    private static <E extends A & B> long sum(List<E> list) {
+        return list.stream().map(E::getName).count() + list.stream().map(E::getNum).count();
+    }
 
-  // https://bugs.java.com/bugdatabase/view_bug.do?bug_id=8142476
-  @Test
-  public void testMapWithGeneric() {
-    List<C> list = Arrays.asList(new CImpl(), new CImpl());
-    getNames(list);
-    getNames(Arrays.asList(new D(), new D()));
-  }
+    // https://bugs.java.com/bugdatabase/view_bug.do?bug_id=8142476
+    // 旧版本JDK 1.8 运行时会报错，但是 1.8.402 也没报错了
+    @Test
+    public void testMapWithGeneric() {
+        List<C> list = Arrays.asList(new CImpl(), new CImpl());
+        System.out.println(sum(list));
+        System.out.println(sum(Arrays.asList(new Combine(), new Combine())));
+    }
 }
 
 
 interface A {
 
-  String getName();
+    String getName();
 }
 
 interface B {
 
-  Integer getNum();
+    Integer getNum();
 }
 
 interface C extends A, B {
@@ -48,26 +50,26 @@ interface C extends A, B {
 
 class CImpl implements C {
 
-  @Override
-  public String getName() {
-    return null;
-  }
+    @Override
+    public String getName() {
+        return null;
+    }
 
-  @Override
-  public Integer getNum() {
-    return null;
-  }
+    @Override
+    public Integer getNum() {
+        return null;
+    }
 }
 
-class D implements A, B {
+class Combine implements A, B {
 
-  @Override
-  public String getName() {
-    return null;
-  }
+    @Override
+    public String getName() {
+        return null;
+    }
 
-  @Override
-  public Integer getNum() {
-    return null;
-  }
+    @Override
+    public Integer getNum() {
+        return null;
+    }
 }
---
+++ java8/src/test/java/com/github/kuangcp/stream/bug/StreamGenericTest.java
@@ -0,0 +1,61 @@
+package com.github.kuangcp.stream.bug;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+
+/**
+ * @author <a href="https://github.com/kuangcp">Kuangcp</a> on 2024-07-10 00:06
+ */
+public class StreamGenericTest {
+
+    @Test
+    public void testList() throws Exception {
+        final ArrayList list = buildSpecialList();
+        final Object result = list.stream()
+                .filter(v -> Objects.equals(((HashMap) v).get("name"), "target"))
+                .map(v -> ((HashMap) v).get("id"))
+                .findAny()
+                // 此处的类型转换没有起到作用，只推断为Object类型
+                .map(v -> (Integer) v)
+                // 即使是硬编码转换类型 同样会只推断为Object类型
+                .map(v -> Integer.parseInt(v.toString()))
+                // 甚至换成任意对象值，都不符合Optional.map方法的签名(泛型推断)，永远推断为Object
+                .map(v -> new HashMap<>())
+                .orElse(null);
+
+        // 只能在lambda表达式结束后做强转
+        Integer resultId = (Integer) result;
+        System.out.println(resultId);
+
+        // 当发起Stream的集合在显式定义泛型后，Optional和Stream的 map方法的泛型就全部正常了。
+        final ArrayList<Object> list2 = buildSpecialList();
+        final Integer resultId2 = list2.stream()
+                .filter(v -> Objects.equals(((HashMap) v).get("name"), "target"))
+                .map(v -> ((HashMap) v).get("id"))
+                .findAny()
+                // 此处的类型转换没有起到作用，修改值也是不起作用，永远返回Object类型
+                .map(v -> (Integer) v)
+                .orElse(null);
+        System.out.println(resultId2);
+    }
+
+    private static ArrayList buildSpecialList() {
+        final ArrayList list = new ArrayList();
+
+        final HashMap item = new HashMap();
+        item.put("name", "target");
+        item.put("id", 12);
+        list.add(item);
+
+
+        final HashMap item2 = new HashMap();
+        item2.put("name", "other");
+        item2.put("id", 1);
+        list.add(item2);
+        return list;
+    }
+
+}
