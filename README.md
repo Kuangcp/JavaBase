de0ab98f4c26fb7fe091a26a8e557622b3b920a2
kuangcp <kuangcp@aliyun.com>
2024-11-06 14:09:54 +0800

    fmt

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# Java 基础学习
+<p align="center"><img width=120 src="https://www.svgrepo.com/show/184143/java.svg"></p>
+<h3 align="center">Java 基础</h3>
 
-[![Java Version](https://img.shields.io/badge/JDK-Java%208-red.svg)](https://www.java.com/zh_CN/download/)
-[![Gradle 5.3](https://img.shields.io/badge/Gradle-5.3-green.svg)](https://docs.gradle.org/5.3/userguide/userguide.html)
-[![996.icu](https://img.shields.io/badge/link-996.icu-red.svg)](https://996.icu)
-[![codebeat badge](https://codebeat.co/badges/9145f9a8-a1aa-4c67-bb2b-f9dd12e924d4)](https://codebeat.co/projects/github-com-kuangcp-javabase-master)
-[![Maintainability](https://api.codeclimate.com/v1/badges/23134c0d2348845fecec/maintainability)](https://codeclimate.com/github/Kuangcp/JavaBase/maintainability)
+<p align="center">
+<a href="https://www.java.com/zh_CN/download/"><img src="https://img.shields.io/badge/JDK-Java%208-red.svg"></a>
+<a href="https://996.icu"><img src="https://img.shields.io/badge/Dev-996.icu-red.svg"></a>
+<a href="https://codebeat.co/projects/github-com-kuangcp-javabase-master"><img src="https://codebeat.co/badges/9145f9a8-a1aa-4c67-bb2b-f9dd12e924d4"></a>
+<a href="https://codeclimate.com/github/Kuangcp/JavaBase/maintainability"><img src="https://api.codeclimate.com/v1/badges/23134c0d2348845fecec/maintainability"></a>
+</p>
 
 ************************
 
@@ -14,8 +16,6 @@
 |   [Java8](/java8)   |   [网络](/network)   |  [Guava](/guava)   |
 |  [算法](/algorithms)  | [设计模式](/pattern) | [Spring](/spring)  |
 |  [Java Gui](/gui)   |  [测试](/test)   | [业务问题](/question)  |
-
-
 
 
 ************************
--- web/src/test/java/com/github/kuangcp/validation/BeanValidatorTest.java
+++ web/src/test/java/com/github/kuangcp/validation/BeanValidatorTest.java
@@ -7,45 +7,45 @@ import org.junit.Test;
 @Slf4j
 public class BeanValidatorTest {
 
-  @Test
-  public void testValid() {
-    TestParam param = new TestParam();
-    param.setState(ActiveState.ACTIVE);
-    param.setStr(StrState.SECOND);
-    BeanValidator.check(param);
-  }
-
-  @Test
-  public void testInvalid() {
-    TestParam param = new TestParam();
-    param.setState(-1);
-    param.setStr(StrState.SECOND);
-    try {
-      BeanValidator.check(param);
-      Assert.fail();
-    } catch (Exception e) {
-      Assert.assertEquals(e.getMessage(), ErrorMsgConstant.STATE_ERROR);
+    @Test
+    public void testValid() {
+        TestParam param = new TestParam();
+        param.setState(ActiveState.ACTIVE);
+        param.setStr(StrState.SECOND);
+        BeanValidator.check(param);
     }
-  }
 
-  @Test
-  public void testValidStr() {
-    TestParam param = new TestParam();
-    param.setStr(StrState.SECOND);
-    param.setState(ActiveState.NONE);
-    BeanValidator.check(param);
-  }
-
-  @Test
-  public void testInvalidStr() {
-    TestParam param = new TestParam();
-    param.setStr("2s");
-    param.setState(3);
-    try {
-      BeanValidator.check(param);
-      Assert.fail();
-    } catch (Exception e) {
-      Assert.assertEquals(e.getMessage(), ErrorMsgConstant.STR_STATE_ERROR);
+    @Test
+    public void testInvalid() {
+        TestParam param = new TestParam();
+        param.setState(-1);
+        param.setStr(StrState.SECOND);
+        try {
+            BeanValidator.check(param);
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertEquals(e.getMessage(), ErrorMsgConstant.STATE_ERROR);
+        }
     }
-  }
+
+    @Test
+    public void testValidStr() {
+        TestParam param = new TestParam();
+        param.setStr(StrState.SECOND);
+        param.setState(ActiveState.NONE);
+        BeanValidator.check(param);
+    }
+
+    @Test
+    public void testInvalidStr() {
+        TestParam param = new TestParam();
+        param.setStr("2s");
+        param.setState(3);
+        try {
+            BeanValidator.check(param);
+            Assert.fail();
+        } catch (Exception e) {
+            Assert.assertEquals(e.getMessage(), ErrorMsgConstant.STR_STATE_ERROR);
+        }
+    }
 }
