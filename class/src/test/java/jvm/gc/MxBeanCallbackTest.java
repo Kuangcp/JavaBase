ddce76438f4439fadc2626add947bce71c43224a
kuangcp <kuangcp@aliyun.com>
2024-12-12 11:48:33 +0800

    gc

--- class/src/test/java/jvm/gc/MxBeanCallbackTest.java
+++ class/src/test/java/jvm/gc/MxBeanCallbackTest.java
@@ -1,25 +1,29 @@
 package jvm.gc;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
 import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
 import java.util.concurrent.TimeUnit;
 
 /**
  * @author <a href="https://github.com/kuangcp">Kuangcp</a> on 2024-03-30 10:50
  */
+@Slf4j
 public class MxBeanCallbackTest {
 
+    /**
+     * jcmd pid GC.run 触发GC，可以看到对应输出
+     */
     @Test
     public void testInstallGCMonitoring() throws Exception {
-
         MxBeanCallback.installGCMonitoring();
+        log.info("finish install");
 
-        ManagementFactory.getMemoryMXBean();
+        MemoryMXBean bean = ManagementFactory.getMemoryMXBean();
+        log.info("bean={}", bean);
 
-
-        System.out.println("xx");
         TimeUnit.MINUTES.sleep(3);
-
     }
 }
